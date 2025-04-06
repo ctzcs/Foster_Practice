9c2a7a25e318351708575f8edf0b2e169a231911
tianwen <yaohii@foxmail.com>
2025-04-07 03:14:17 +0800

    构建跨平台自定义shader

---
+++ Content/Assets/Shader/CustomShader.cs
@@ -0,0 +1,24 @@
+﻿using Engine.Source.Utility;
+using Foster.Framework;
+
+
+namespace Content.Assets.Shader;
+
+public class CustomShader(GraphicsDevice graphicsDevice):
+    Foster.Framework.Shader(graphicsDevice,GetCreateInfo(graphicsDevice),"Custom")
+{
+    private static ShaderCreateInfo GetCreateInfo(GraphicsDevice graphicsDevice) => new(
+        Vertex:new(
+            Code:EnginePlatform.ReadEmbeddedBytes<CustomShader>($"Custom.vertex.{graphicsDevice.Driver.GetShaderExtension()}"),
+            SamplerCount:0,
+            UniformBufferCount:1,
+            EntryPoint:"vertex_main"
+            ),
+        Fragment:new(
+            Code:EnginePlatform.ReadEmbeddedBytes<CustomShader>($"Custom.fragment.{graphicsDevice.Driver.GetShaderExtension()}"),
+            SamplerCount:1,
+            UniformBufferCount:0,
+            EntryPoint:"fragment_main"
+            )
+        );
+}
